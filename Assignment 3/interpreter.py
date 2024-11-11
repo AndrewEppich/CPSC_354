14f049ff4a77b66e6bad7cf424595b9eabb27dfd
AndrewEppich <eppich@chapman.edu>
2024-11-10 20:48:24 -0800

    Homework 11

--- Assignment 3/interpreter.py
+++ Assignment 3/interpreter.py
@@ -1,11 +1,12 @@
 import sys
 from lark import Lark, Transformer, Tree
 import lark
+import os
 
-print(f"Python version: {sys.version}")
-print(f"Lark version: {lark.__version__}")
+#print(f"Python version: {sys.version}")
+#print(f"Lark version: {lark.__version__}")
 
-
+#  run/execute/interpret source code
 def interpret(source_code):
     cst = parser.parse(source_code)
     ast = LambdaCalculusTransformer().transform(cst)
@@ -13,10 +14,10 @@ def interpret(source_code):
     result = linearize(result_ast)
     return result
 
-
+# convert concrete syntax to CST
 parser = Lark(open("grammar.lark").read(), parser='lalr')
 
-
+# convert CST to AST
 class LambdaCalculusTransformer(Transformer):
     def lam(self, args):
         name, body = args
@@ -33,50 +34,54 @@ class LambdaCalculusTransformer(Transformer):
     def NAME(self, token):
         return str(token)
 
-
+# reduce AST to normal form
 def evaluate(tree):
     if tree[0] == 'app':
         e1 = evaluate(tree[1])
-        e2 = evaluate(tree[2])
-
-        if e1 == e2:
-            return tree 
-
         if e1[0] == 'lam':
             body = e1[2]
             name = e1[1]
-            rhs = substitute(body, name, e2)
+            arg = tree[2]
+            rhs = substitute(body, name, arg)
             result = evaluate(rhs)
+            pass
         else:
-            result = ('app', e1, e2)
+            result = ('app', e1, tree[2])
+            pass
     else:
         result = tree
+        pass
     return result
 
+# generate a fresh name 
+# needed eg for \y.x [y/x] --> \z.y where z is a fresh name)
 class NameGenerator:
     def __init__(self):
         self.counter = 0
 
     def generate(self):
         self.counter += 1
+        # user defined names start with lower case (see the grammar), thus 'Var' is fresh
         return 'Var' + str(self.counter)
 
 name_generator = NameGenerator()
 
+# for beta reduction (capture-avoiding substitution)
+# 'replacement' for 'name' in 'tree'
 def substitute(tree, name, replacement):
+    # tree [replacement/name] = tree with all instances of 'name' replaced by 'replacement'
     if tree[0] == 'var':
         if tree[1] == name:
-            return replacement
+            return replacement # n [r/n] --> r
         else:
-            return tree
+            return tree # x [r/n] --> x
     elif tree[0] == 'lam':
         if tree[1] == name:
-            return tree
+            return tree # \n.e [r/n] --> \n.e
         else:
             fresh_name = name_generator.generate()
-            new_body = substitute(tree[2], tree[1], ('var', fresh_name))
-            result = ('lam', fresh_name, substitute(new_body, name, replacement))
-            return result
+            return ('lam', fresh_name, substitute(substitute(tree[2], tree[1], ('var', fresh_name)), name, replacement))
+            # \x.e [r/n] --> (\fresh.(e[fresh/x])) [r/n]
     elif tree[0] == 'app':
         return ('app', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
     else:
@@ -90,19 +95,27 @@ def linearize(ast):
     elif ast[0] == 'app':
         return "(" + linearize(ast[1]) + " " + linearize(ast[2]) + ")"
     else:
-        raise Exception('Unknown AST', ast)
+        return ast
 
 def main():
+    import sys
     if len(sys.argv) != 2:
-        print("Usage: python interpreter.py <filename>", file=sys.stderr)
+        #print("Usage: python interpreter.py <filename or expression>", file=sys.stderr)
         sys.exit(1)
 
-    filename = sys.argv[1]
-    with open(filename, 'r') as file:
-        expression = file.read()
+    input_arg = sys.argv[1]
+
+    if os.path.isfile(input_arg):
+        # If the input is a valid file path, read from the file
+        with open(input_arg, 'r') as file:
+            expression = file.read()
+    else:
+        # Otherwise, treat the input as a direct expression
+        expression = input_arg
 
     result = interpret(expression)
     print(f"\033[95m{result}\033[0m")
 
 if __name__ == "__main__":
     main()
+    
